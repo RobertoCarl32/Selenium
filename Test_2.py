68b0d118305e0920b558c1bd52f05725b6d07227
RobertoCarl32 <castillo.roberto@correounivalle.edu.co>
2025-08-18 12:45:58 -0500

    Guardando las pruebas

---
+++ Test_2.py
@@ -0,0 +1,49 @@
+import platform
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+# Detectar sistema operativo con so
+so = platform.system()
+
+options = Options()
+
+if so == "Windows":
+    # Configuración para Windows con Chrome
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    service = Service(r"C:\chromedriver-win64\chromedriver.exe")
+
+elif so == "Linux":
+    # Configuración para Debian con Brave
+    options.binary_location = "/usr/bin/brave-browser"
+    service = Service("/usr/bin/chromedriver")
+
+else:
+    raise Exception(f"Sistema operativo no soportado: {so}")
+
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://demoqa.com/text-box")
+driver.maximize_window()
+time.sleep(2)
+nom = driver.find_element(By.ID, "userName")
+nom.send_keys("Rodrigo")
+time.sleep(1)
+driver.find_element(By.ID, "userName").send_keys("rodrigo@gmail.com")
+time.sleep(1)
+driver.find_element(By.ID, "currentAddress").send_keys("Direccion uno")
+time.sleep(1)
+driver.find_element(By.ID, "permanentAddress").send_keys("Direccion dos")
+time.sleep(1)
+
+driver.execute_script("window.scrollTo(0, 500)")
+time.sleep(2)
+
+driver.find_element(By.ID, "submit").click()
+time.sleep(2)
+
+
+driver.close()
